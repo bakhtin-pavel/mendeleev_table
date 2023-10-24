e8be294f8cc389b6d5ce36c2bd278a2db7f7e55d
bakhtin-pavel <p1avel.com@gmail.com>
2023-10-24 12:23:43 +0700

    save 24.10

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Layout, Main } from './pages';
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,6 +3,8 @@ import styles from './Main.module.scss';
 
 import { useLocation } from 'react-router-dom';
 
+import { Helmet } from 'react-helmet';
+
 import { ELEMENTS_DATA } from './data';
 
 import { Icon } from '../../assets/icon';
@@ -13,28 +15,28 @@ const Main = () => {
     const lastPart = pathname.split('/').pop();
 
     return (
-        <main>
-            <div style={{ backgroundColor: ELEMENTS_DATA[lastPart].color }} className={styles.headBlock}>
-                <div className={styles.leftHeadBlock}>
-                    <p className={ELEMENTS_DATA[lastPart].abbreviation.length === 1 ? styles.abbreviation_one : styles.abbreviation_many}>{ELEMENTS_DATA[lastPart].abbreviation}</p>
-                    <h1>{ELEMENTS_DATA[lastPart].name}</h1>
-                    <p className={styles.translate_name}>{ELEMENTS_DATA[lastPart].translate_name}</p>
-                    <p className={styles.atomic_mass}>|{ELEMENTS_DATA[lastPart].atomic_mass}|</p>
+        <>
+            <Helmet>
+                <title>{ELEMENTS_DATA[lastPart].name}</title>
+            </Helmet>
+            <main>
+                <div style={{ backgroundColor: ELEMENTS_DATA[lastPart].color }} className={styles.headBlock}>
+                    <div className={styles.leftHeadBlock}>
+                        <p className={lastPart.length === 1 ? styles.abbreviation_one : styles.abbreviation_many}>{lastPart}</p>
+                        <h1>{ELEMENTS_DATA[lastPart].name}</h1>
+                        <p className={styles.translate_name}>{ELEMENTS_DATA[lastPart].translate_name}</p>
+                        <p className={styles.atomic_mass}>|{ELEMENTS_DATA[lastPart].atomic_mass}|</p>
+                    </div>
+                    <div className={styles.rightHeadBlock}>
+                        <Icon />
+                        <p>{ELEMENTS_DATA[lastPart].serial_number}</p>
+                    </div>
                 </div>
-                <div className={styles.rightHeadBlock}>
-                    <Icon />
-                    <p>{ELEMENTS_DATA[lastPart].serial_number}</p>
-                </div>
-            </div>
-            <section className={styles.infoBlock}>
-                <h3>Химический элемент</h3>
-                <p>{ELEMENTS_DATA[lastPart].chemical_element}</p>
-                <h3>Устройство</h3>
-                <p>{ELEMENTS_DATA[lastPart].structure}</p>
-                <h3>Минерал</h3>
-                <p>{ELEMENTS_DATA[lastPart].mineral}</p>
-            </section>
-        </main>
+                <section className={styles.infoBlock}>
+                    <p>{ELEMENTS_DATA[lastPart].text}</p>
+                </section>
+            </main>
+        </>
     )
 }
 
